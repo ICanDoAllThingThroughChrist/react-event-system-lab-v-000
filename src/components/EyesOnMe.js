1304a5d31199dc96d77bb7ba4e690f243194642b
charlie  lee <leetochia@gmail.com>
2019-12-09 00:04:33 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,2 +1,12 @@
 // Code EyesOnMe Component Here
 import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+  render() {
+    return (
+      <button onFocus={this.p} onBlur=
+  {this.p1}>button</button>
+    )
+  }
+}
